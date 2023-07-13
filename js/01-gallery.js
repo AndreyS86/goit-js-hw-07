ef8c9412d26c0aad441be6dedea326a27881bf4b
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2023-07-13 14:44:48 +0300

    process

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
 
 const galleryUl = document.querySelector(".gallery");
 const markup = createGalleryItems(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryUlLb = document.querySelector(".gallery");
+const markupLb = createGalleryItems(galleryItems);
+
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+    })
+    .join("");
+}
+
+galleryUlLb.insertAdjacentHTML("beforeend", markupLb);
+galleryUl.addEventListener("click", handleShow);
+
+function handleShow(e) {
+    e.preventDefault()
+}
+
+// let Lightbox = new SimpleLightbox(".gallery a", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
